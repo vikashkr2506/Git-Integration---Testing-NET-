7c9e5538234612ad41da50a2a4021659afb814cc
Vikash Kumar <vikash.kumar@mathco.com>
2025-08-06 18:41:26 +0530

    Widget code sync from db

---
+++ NET - Git Integration Test/screens/2__Screen 3/widgets/0_widget/visual_uiac.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from codex_widget_factory_lite.visuals.simple_table import SimpleTable
+# Hardcoding a sample dataframe here, please ingest you dataset or create
+sample_df = pd.DataFrame(data = [['tom','US',10], ['nick','US',15],
+    ['juli','US',14]], columns=['Name','country','Age'])
+simpletable_output= SimpleTable(df = sample_df, show_searchbar=True,freezing=True)
+simpletable_output.add_tooltip(isTooltip=True,tooltip_text="This is a tooltip",placement="top")
+dynamic_outputs = simpletable_output.json_string
